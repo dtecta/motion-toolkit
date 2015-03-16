b3bd9e19ac26bddb9587a78419a71da463087b6a
Gino van den Bergen <gino@dtecta.com>
2015-03-16 18:08:46 +0100

    Some more copyright notices

--- guts/BinomialQueue.hpp
+++ guts/BinomialQueue.hpp
@@ -5,19 +5,6 @@
 	For details please see COPYING file or visit 
 	http://opensource.org/licenses/MIT
 */
-/*
- * GUTS - Generic Utilities 
- * Copyright (c) 2006, Gino van den Bergen, DTECTA 
- * http://www.dtecta.com
- *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Gino van den Bergen makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- */
 
 #ifndef GUTS_BINOMIALQUEUE_HPP
 #define GUTS_BINOMIALQUEUE_HPP
